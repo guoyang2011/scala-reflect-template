8b8d56dbc0df99669754aa19ee849339c4ac2bba
guoyang2011 <guoyang2011@gmail.com>
2015-03-09 17:57:08 +0800

    init.

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "Scala-Reflect-Template"
+
+version := "1.0"
+
+libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.10.4" withSources() withJavadoc()
+
+libraryDependencies +="org.jblas" % "jblas" % "1.2.3"
---
+++ src/main/scala/cn/changhong/reflect/util/template.scala
@@ -0,0 +1,55 @@
+package cn.changhong.reflect.util
+
+import org.jblas.DoubleMatrix
+
+import scala.reflect.ClassTag
+import scala.reflect.runtime.{currentMirror=>cm}
+import scala.reflect.runtime.universe._
+/**
+ * Created by yangguo on 15-3-9.
+ */
+object NewInstance{
+  def apply[A](args:Map[String,Any])(implicit t:ClassTag[A]):A={
+    val claas=cm.classSymbol(t.runtimeClass)
+    val module=claas.companionSymbol.asModule
+    val im=cm.reflect(cm.reflectModule(module).instance)
+    newInstance[A](im,"apply",args)
+  }
+  private[this] def newInstance[A](im:InstanceMirror,methodName:String,values:Map[String,Any]):A= {
+    val at = newTermName(methodName)
+    val ts = im.symbol.typeSignature
+    val method = (ts.member(at)).asMethod
+    val args=(for (pp <- method.paramss; p <- pp) yield p).map { p =>
+      val value = values.get(p.name.toString)
+      if (p.typeSignature <:< typeOf[Option[_]]) value
+      else {
+        if (!value.isEmpty) value.get
+        else {
+          try{
+            defaultVal(p)
+          }catch{
+            case ex:Throwable=>throw new IllegalArgumentException(s"TypeSafe Error!${method.fullName} ,Argument Name[${p.name.toString}] Is Null!")
+          }
+        }
+      }
+    }
+    (im.reflectMethod(method))(args:_*).asInstanceOf[A]
+  }
+  private[this] def defaultVal(symbol:Symbol)={
+    symbol.typeSignature match{
+      case t if t <:< typeOf[Int]||t<:<typeOf[Double]||t<:<typeOf[Long]||t<:<typeOf[Float]||t<:<typeOf[Char]=>0
+      case t if t <:< typeOf[String]=>""
+      case t=> throw new IllegalArgumentException("Type Safe Error!")
+    }
+  }
+ }
+object Test1{
+  case class Person(name:String,age:Int,sex:Option[String])
+  def main (args: Array[String]) ={
+    val start=System.currentTimeMillis()
+    val p=NewInstance[Person](Map("name"->""))
+    val res=(System.currentTimeMillis()-start)
+    println(p.age+p.name)
+    res
+  }
+}
